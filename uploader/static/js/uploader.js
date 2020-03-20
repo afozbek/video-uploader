d77674b918786b439d3655b9c5207ac7104a63a6
Furkan Ozbek <furkanozbek1995@gmail.com>
2020-03-20 18:01:40 +0300

    file temizleme için script de güncelleme yapıldı

--- uploader/static/js/uploader.js
+++ uploader/static/js/uploader.js
@@ -1,3 +1,7 @@
 console.log("Furkan Browser'a selamlarını iletir...");
 
 // Yeni scriptler gelicektir
+const video_file = document.querySelector("#video_file");
+
+// Clear Video File
+video_file.value = "";
