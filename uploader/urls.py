f034994d434befbe7de660c4258e65c0f93f1e79
Furkan Ozbek <furkanozbek1995@gmail.com>
2020-03-20 17:57:16 +0300

    commentler eklendi

--- uploader/urls.py
+++ uploader/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /
     path('', views.index, name='index'),
+    # /upload
     path('upload', views.upload, name='upload'),
 ]
